bef1c7ecee0346f28d545f2993d8e67548b0c307
Micheale Abay <michealeabay@Micheales-MacBook-Air.local>
2023-02-10 00:41:30 -0500

    removed port from env file

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../build/index.html"));
+  res.send("Welcome to friends!");
 });
 
 // Mongoose Setup
